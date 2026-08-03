e56d6a5d9b30d0c8e1901ec075b979522379bdeb
ansilanuuu777 <muhammedansil7777@gmail.com>
2026-08-03 16:11:12 +0530

    Remove automatic RTO API call on Edit, load 100 percent from database

--- lib/car_reg_form.dart
+++ lib/car_reg_form.dart
@@ -89,12 +89,6 @@ class _CarFormPageState extends State<CarFormPage> {
     _initializeControllers();
     fetchDriverDetails();
     fetchCarCategories();
-
-    if (widget.carData != null && _controllers['rc_no']!.text.isNotEmpty) {
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        _onVerifyRc();
-      });
-    }
   }
 
   Future<void> fetchCarCategories() async {
